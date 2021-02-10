4e69a6f70afe398e834d9cc46e422e017624e909
Yohan Poirier-Ginter <pg.yohan@gmail.com>
2021-02-10 15:51:15 -0500

    added different configs

--- train_lsunchurch_baseline.sh
+++ train_lsunchurch_baseline.sh
@@ -13,4 +13,4 @@ source $HOME/genforce-env/bin/activate
 GPUS=4
 CONFIG=configs/stylegan_lsunchurch256.py
 WORK_DIR=work_dirs/stylegan_lsunchurch256_train
-./scripts/dist_train.sh ${GPUS} ${CONFIG} ${WORK_DIR} --adain_type BlockwiseAdaIN
+./scripts/dist_train.sh ${GPUS} ${CONFIG} ${WORK_DIR} --adain_type StyleModLayer
