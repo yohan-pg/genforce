587e20c0f1641462b7c5b97b91b9bb12b6c26144
Yohan Poirier-Ginter <pg.yohan@gmail.com>
2021-02-10 13:06:15 -0500

    one more typo -.-

--- train_lsunchurch.sh
+++ train_lsunchurch.sh
@@ -3,6 +3,6 @@
 source $HOME/genforce-env/bin/activate
 
 GPUS=2
-CONFIG=configs/stylegan_lsunchruch256.py
-WORK_DIR=work_dirs/stylegan_lsunchruch256_train
+CONFIG=configs/stylegan_lsunchurch256.py
+WORK_DIR=work_dirs/stylegan_lsunchurch256_train
 ./scripts/dist_train.sh ${GPUS} ${CONFIG} ${WORK_DIR}
